de15ba5a43a8c76faa8b42f942dca8d0b89e595a
tutugodfrey <30448913+tutugodfrey@users.noreply.github.com>
2018-08-09 20:21:10 +0100

    Delete Label.jsx

--- client/src/components/elementComponents/Label.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class Label extends Component {
-  render() {
-    return <label id={this.props.labelId} for={this.props.labelFor}>
-      {this.props.content}
-    </label>
-  }
-}
