d0f27e860dbb3337fd60d6a1954b0c7f377100a8
tutugodfrey <30448913+tutugodfrey@users.noreply.github.com>
2018-08-09 20:24:47 +0100

    Delete Link.jsx

--- client/src/components/elementComponents/Link.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class Link extends Component {
-  render() {
-    return <a href={this.props.href} id={this.props.linkId} className={this.props.linkClass} onClick={this.props.onClick} >
-    {this.props.linkText}
-    </a>
-  }
-}
